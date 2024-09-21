fa9e33f41e51cc171468d24235840351d44cd01e
Andra <111245270+Andra-sun@users.noreply.github.com>
2024-09-21 16:27:05 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('https://classapi-production.up.railway.app/all_users')
-        .then(response => {
+    fetch("https://classapi-production.up.railway.app/all_users")
+        .then((response) => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.json();
         })
-        .then(data => {
-            const container = document.getElementById('listaContainer');
-            container.innerHTML = ''; 
+        .then((data) => {
+            const container = document.getElementById("listaContainer");
+            container.innerHTML = "";
 
-            data.forEach(profile => {
-                const profileCard = document.createElement('div');
-                profileCard.className = 'profileCard';
+            data.forEach((profile) => {
+                const profileCard = document.createElement("div");
+                profileCard.className = "profileCard";
 
-                
-                const directImageLink = `https://classapi-production.up.railway.app/photo/${profile.ID}`; 
+                const directImageLink = `https://classapi-production.up.railway.app/photo/${profile.ID}`;
 
                 profileCard.innerHTML = `
                     <img class="ProfileImage" src="${directImageLink}" alt="avatar">
@@ -24,14 +23,37 @@ document.addEventListener("DOMContentLoaded", function () {
                         <p class="nickname">${profile.Apelido}</p>
                     </div>
                     <div class="buttons">
-                        <a class="linkedin" href="${profile.Linkedin}" target="_blank"><i class="fi fi-brands-linkedin"></i></a>
-                        <a class="instagram" href="${profile.Instagram}" target="_blank"><i class="fi fi-brands-instagram"></i></a>
-                        <a class="github" href="${profile.Github}" target="_blank"><i class="fi fi-brands-github"></i></a>
+                        <a class="linkedin" href="${
+                            profile.Linkedin
+                        }" target="_blank" style="display: ${
+                    isValidUrl(profile.Linkedin) ? "block" : "none"
+                };">
+                            <i class="fi fi-brands-linkedin"></i>
+                        </a>
+                        <a class="instagram" href="${
+                            profile.Instagram
+                        }" target="_blank" style="display: ${
+                    isValidUrl(profile.Instagram) ? "block" : "none"
+                };">
+                            <i class="fi fi-brands-instagram"></i>
+                        </a>
+                        <a class="github" href="${
+                            profile.Github
+                        }" target="_blank" style="display: ${
+                    isValidUrl(profile.Github) ? "block" : "none"
+                };">
+                            <i class="fi fi-brands-github"></i>
+                        </a>
                     </div>
                 `;
 
                 container.appendChild(profileCard);
             });
+
+            // funcao para verificar se o url e valido
+            function isValidUrl(url) {
+                return /^(https?:\/\/)/.test(url);
+            }
         })
-        .catch(error => console.error('Error loading JSON:', error));
+        .catch((error) => console.error("Error loading JSON:", error));
 });
