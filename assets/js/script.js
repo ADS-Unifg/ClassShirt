e799d3273c5d26aa0a9da3ac386a34ac3d759e4a
Andra <111245270+Andra-sun@users.noreply.github.com>
2024-09-21 16:26:18 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,7 @@
 //formulario
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
     const formulario = document.getElementById("formulario");
-    formulario.addEventListener("click", function() {
+    formulario.addEventListener("click", function () {
         window.open("https://classapi-production.up.railway.app/", "_blank");
     });
 });
-
-
-//lista
-// const lista = document.getElementById("lista");
-// const listaContainer = document.getElementById("listaContainer");
-
-// lista.addEventListener("click", function() {
-//     if (listaContainer.style.display === "flex") {
-//         listaContainer.style.display = "none";
-//     } else {
-//         listaContainer.style.display = "flex";
-//     }
-// });
-
-
-//icon link
-//ainda em teste!!!!!!!!!
-document.addEventListener("DOMContentLoaded", function () {
-
-    function updateButtonsVisibility() {
-        document.querySelectorAll('.buttons').forEach((profileElement) => {
-            const linkedinLink = profileElement.querySelector('.linkedin');
-            const instagramLink = profileElement.querySelector('.instagram');
-            const githubLink = profileElement.querySelector('.github');
-
-            function isValidUrl(url) {
-                return url && url !== '.' && url.trim() !== '';
-            }
-
-            linkedinLink.style.display = isValidUrl(linkedinLink.getAttribute('href')) ? 'flex' : 'none';
-            instagramLink.style.display = isValidUrl(instagramLink.getAttribute('href')) ? 'flex' : 'none';
-            githubLink.style.display = isValidUrl(githubLink.getAttribute('href')) ? 'flex' : 'none';
-
-            instagramLink.style.color = "red !important";
-        });
-    }
-
-    updateButtonsVisibility();
-});
-
-
-
-
-
-
-//home
-
-
