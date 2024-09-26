34ca8410535979bb4cc73a770b75e4bc473d33c8
wendellast <wendelalves01a@hotmail.com>
2024-09-26 20:30:54 -0300

    edit:user

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
         window.open("https://classapi-production.up.railway.app/", "_blank");
     });
 });
+
+//edit
+document.addEventListener("DOMContentLoaded", (event) => {
+    const formulario = document.getElementById("edit");
+    formulario.addEventListener("click", function () {
+        window.open("https://classapi-production.up.railway.app/edit_user/", "_blank");
+    });
+});
