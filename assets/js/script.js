5a8995610bdb8d6990951d99b4e644d67b1a2fbe
wendellast <wendelalves01a@hotmail.com>
2024-09-26 22:51:23 -0300

    delete-button

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
         window.open("https://classapi-production.up.railway.app/edit_user/", "_blank");
     });
 });
+
+//delete
+document.addEventListener("DOMContentLoaded", (event) => {
+    const formulario = document.getElementById("delete");
+    formulario.addEventListener("click", function () {
+        window.open("https://classapi-production.up.railway.app/delete_user/", "_blank");
+    });
+});
+
